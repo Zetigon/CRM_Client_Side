511c02303b9fc1c03c84385d8739eff888e85deb
OlwethuN <olwethun@zetigon.com>
2023-03-14 16:59:30 +0200

    commit on master on route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
+import createRoot from "react-dom";
 
-
+import NavBar from "../src/Components/NAVIGATION/NAVBAR/Navbar";
+import PublicRoutes from ''
 
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
- 
-      </header>
+   <NavBar/>
     </div>
   );
 }
