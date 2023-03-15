3fa798534e40eda196d89c6d181ea40372e0f186
OlwethuN <olwethun@zetigon.com>
2023-03-15 17:23:36 +0200

    update: app and components with new packages

--- src/App.js
+++ src/App.js
@@ -1,49 +1,37 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+import store, { persistor } from './store'
+import Theme from 'components/template/Theme'
+import Layout from 'components/layout'
+import history from './history'
+import mockServer from './mock'
+import appConfig from 'configs/app.config'
+import './locales'
+
+const environment = process.env.NODE_ENV
+
 /**
- * @author Firstname Lastname <firstname.lastname@example.com>
- * @file Description
- * @desc Created on 2023-03-14 5:37:47 pm
- * @copyright APPI SASU
+ * Set enableMock(Default false) to true at configs/app.config.js
+ * If you wish to enable mock api
  */
-import './App.css';
-import Home from './Components/HOME/home';
-import { Routes, Route } from "react-router-dom";
-
-import { DynamicItem, Sidebar, dummyData } from "./Components/NAVIGATION/";
-import Crm from './Components/CRM/crm';
-import Dashboard from './Components/DASHBOARD/Dashboard';
-import Accounts from './Components/CRM/Accounts/accounts';
-import Notes from './Components/CRM/Notes/notes';
-import Tasks from './Components/CRM/Tasks/tasks';
-import Settings from './Components/SETTINGS/settings';
-
-function App() {
-  return (
-      <div id="main">
-      <Sidebar>
-        <Routes>
-          <Route path="/" element={<Dashboard/>} />
-            <Route path="/crm" element={<Crm/>}/>
-            <Route path="/accounts" element={<Accounts/>}/>
-            <Route path="/notes" element={<Notes/>}/>
-            <Route path="/tasks" element={<Tasks/>}/>
-
-            <Route path="/tasks" element={<Tasks/>}/>
-            <Route path="/settings" element={<Settings/>}/>
-          {dummyData &&
-            dummyData.map((item, index) => (
-              <Route
-                key={index}
-                path={item.path}
-                element={<DynamicItem page = {item.title}/>}
-              />
-            ))}
-
-
-
-        </Routes>
-      </Sidebar>
-    </div>
-  );
+if (environment !== 'production' && appConfig.enableMock) {
+    mockServer({ environment })
 }
 
-export default App;
+function App() {
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter history={history}>
+                    <Theme>
+                        <Layout />
+                    </Theme>
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
+    )
+}
+
+export default App
--- src/Components/NAVIGATION/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Sidebar} from "./SIDEBAR";
-export { default as SidebarItems} from "./SIDEBAR/SidebarItems";
-export { default as DynamicItem } from "./Routes/[item]";
-
-
-
-export { SIDEBAR_DATA as dummyData } from "./Data";
