24b66fc973d2ebb67b098e51c8b9d76177ff84c4
OlwethuN <olwethun@zetigon.com>
2023-03-18 22:43:49 +0200

    commit on master

---
+++ src/views/People/Accounts/accounts.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+
+function accounts() {
+  return (
+
+
+    <div className='container-wrap'>
+
+        <h1 className='text-mute'>
+            <b>Accounts</b>
+        </h1>
+
+      
+
+
+
+
+
+
+
+
+
+
+
+        <h3 className='text-muted'>
+            Customers
+        </h3>
+      
+    </div>
+  )
+}
+
+export default accounts
