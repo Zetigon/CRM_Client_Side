56a01ed2ac677ed085381c880ea7133af4577c0d
OlwethuN <olwethun@zetigon.com>
2023-03-19 01:21:32 +0200

    commit on master: accounts page

--- src/views/People/Accounts/accounts.js
+++ src/views/People/Accounts/accounts.js
@@ -5,31 +5,74 @@ import React from 'react'
 function accounts() {
   return (
 
+<div className='container-wrap'>
 
-    <div className='container-wrap'>
 
-        <h1 className='text-mute'>
-            <b>Accounts</b>
-        </h1>
-
-      
-
+<h3 className='text-mute-center'>
+Accounts
+</h3>
+<br/>
 
 
 
 
+<div className="grid grid-cols-3 gap-3 bg-amber-200">
+
+{/* Total Number of  Accounts */}
+<div className='cards'>
+
+
+<small>Total</small> Accounts
+
+<div className='total-container_grid'>
+<span className='total-container'>
+  457
+</span>
+</div>
+
+</div>
+
+
+
+{/* Active Accounts */}
+
+<div className='cards'>
+<small>Active</small> Accounts
+
+<div className='total-container_grid'>
+<span className='total-container'>
+  1,457
+</span>
+</div>
+
+
+
+
+</div>
+
+
+{/* New Accounts */}
+<div className='cards'>
+<small>New</small> Accounts
+
+<div className='total-container_grid'>
+<span className='total-container'>
+  57
+</span>
+</div>
+
+</div>
 
 
 
 
 
 
-        <h3 className='text-muted'>
-            Customers
-        </h3>
-      
-    </div>
-  )
+</div>
+
+
+</div>
+  );
 }
 
 export default accounts
