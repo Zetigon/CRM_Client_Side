16e61aaae4a7b8a9d914d11452d2caf61d29021e
OlwethuN <olwethun@zetigon.com>
2023-04-18 11:39:09 +0200

    update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,9 +4,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function(app) {
     app.use(
-        '/api',
+        '/',
             createProxyMiddleware({
-            target: 'http://127.0.0.1:8000',  // <--- config the url based on your backend server
+            target: '/',  // <--- config the url based on your backend server
             changeOrigin: true,
         })
     )
